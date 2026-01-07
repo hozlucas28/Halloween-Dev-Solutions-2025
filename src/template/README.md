8ac24f9f2c2f4e34a41f9b3a9fe047d40794f7f8
hozlucas28 <hozlucas28@gmail.com>
2026-01-07 03:28:22 -0300

    chore: upload `template` folder

---
+++ src/template/README.md
@@ -0,0 +1,17 @@
+# Challenge <NÚMERO DEL DESAFÍO>
+
+XXX
+
+```js
+XXX
+```
+
+## Examples
+
+```js
+XXX
+```
+
+## Candies earned
+
+![XXX candies](../../.github/<NÚMERO DEL DESAFÍO>-challenge-candies.png)
---
+++ src/template/solution.spec.ts
@@ -0,0 +1,9 @@
+import fn from './solution'
+
+describe('Challenge <NÚMERO DEL DESAFÍO>: XXX', () => {
+	it('Test XXX', () => {
+		const result = fn()
+		const expected = 'XXX'
+		expect(result).toBe(expected)
+	})
+})
